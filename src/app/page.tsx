ca022304e7d775177132e35546cac2d0c61a4063
felipeclarindo <fglpc2@gmail.com>
2024-10-11 12:06:14 -0300

    Ajustando pagina principal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
-import { RedesSociais } from "@/components/RedesSociais";
+import { Slides } from "@/components/Slides";
+import { SocialMedia } from "@/components/SocialMedia";
 
 export default function Home() {
   return (
-    <main className="h-[80vh] m-w-[100vw] bg-[var(--primary-bg-color)] text-[var(--primary-text-color)]">
-      <section>
-        <h1>Felipe Clarindo</h1>
-        <span>Desenvolvedor Full Stack (Back-end & Front-end)</span>
-        <RedesSociais />
+    <main className="min-h-[70vh] min-w-[100vw] bg-[var(--primary-bg-color)] text-[var(--primary-text-color)]">
+      <section className="flex justify-center items-center max-w-[100vw]">
+        <div className="w-[50%] relative">
+          <h1 className="text-4xl font-bold">Felipe Clarindo</h1>
+          <span className="text-2xl">
+            Desenvolvedor Full Stack (Back-end & Front-end)
+          </span>
+          <SocialMedia />
+        </div>
+        <div className="w-[50%] h-[80vh] relative bg-blue-50">
+          <Slides />
+        </div>
       </section>
       {/* <AboutMe /> */}
     </main>

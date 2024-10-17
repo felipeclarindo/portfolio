3b34a2c8408fb9676c740a818feab7c0a4fa8d2e
felipeclarindo <fglpc2@gmail.com>
2024-10-17 19:10:38 -0300

    Configurando tailwind e next, adicionando types e adicionando dependencias

--- src/types.ts
+++ src/types.ts
@@ -1,19 +1,30 @@
-
 export type TechnologiesType = {
-    id: number;
-    nome: string;
-    image: string;
-  };
-
-export type ProjectProps = {
-    id: number;
-    nome: string;
-    descricao: string;
-    image: string;
-    categoria: string;
-    tecnologias: Array<TechnologiesType>;
-    status: string;
-    dataInicio: string;
-    dataFim: string;
+  id: number;
+  nome: string;
+  image: string;
 };
 
+export type ProjectProps = {
+  id: number;
+  nome: string;
+  descricao: string;
+  image: string;
+  categoria: string;
+  tecnologias: Array<TechnologiesType>;
+  status: string;
+  dataInicio: string;
+  dataFim: string;
+};
+
+export type SlideProps = {
+  id: number;
+  url: string;
+  alt: string;
+  title: string;
+  description: string;
+};
+
+export type LoaderProps = {
+  src: string;
+  width: number;
+};

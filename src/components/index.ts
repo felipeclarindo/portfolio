71345217a23127049c5c436316c52d99a2113d82
felipeclarindo <fglpc2@gmail.com>
2025-03-10 23:40:37 -0300

    feat(components): Start components development.

---
+++ src/components/index.ts
@@ -0,0 +1,9 @@
+import BackgroundAnimated from "./BackgroundAnimated";
+import Footer from "./Footer";
+import Header from "./Header";
+
+module.exports = {
+  BackgroundAnimated,
+  Footer,
+  Header,
+};
